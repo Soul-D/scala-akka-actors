e8d89b08177460db9111940531353e512921bc0f
herb.wks02 <herbert.kreutzer@sdt.co.at>
2017-09-14 13:54:53 +0200

    Device actor write

--- src/main/scala/at/co/sdt/herb/actors/doc/akka/io/iot/Device.scala
+++ src/main/scala/at/co/sdt/herb/actors/doc/akka/io/iot/Device.scala
@@ -5,6 +5,10 @@ import akka.actor.{ Actor, ActorLogging, Props }
 object Device {
   def props(groupId: String, deviceId: String): Props = Props(new Device(groupId, deviceId))
 
+  final case class RecordTemperature(requestId: Long, value: Double)
+
+  final case class TemperatureRecorded(requestId: Long)
+
   final case class ReadTemperature(requestId: Long)
 
   final case class RespondTemperature(requestId: Long, value: Option[Double])
@@ -22,6 +26,11 @@ class Device(groupId: String, deviceId: String) extends Actor with ActorLogging 
   override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
 
   override def receive: Receive = {
+    case RecordTemperature(id, value) =>
+      log.info("Recorded temperature reading {} with {}", value, id)
+      lastTemperatureReading = Some(value)
+      sender() ! TemperatureRecorded(id)
+
     case ReadTemperature(id) =>
       sender() ! RespondTemperature(id, lastTemperatureReading)
   }
