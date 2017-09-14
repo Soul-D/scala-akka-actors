0e59601f732c0a1a6baa3b9d76daee70f60a914e
herb.wks02 <herbert.kreutzer@sdt.co.at>
2017-09-14 13:32:11 +0200

    Device actor read

---
+++ src/test/scala/at/co/sdt/herb/actors/doc/akka/io/iot/DeviceSpec.scala
@@ -0,0 +1,29 @@
+package at.co.sdt.herb.actors.doc.akka.io.iot
+
+import org.scalatest.{ BeforeAndAfterAll, FlatSpecLike, Matchers }
+
+import akka.actor.ActorSystem
+import akka.testkit.{ TestKit, TestProbe }
+
+class DeviceSpec(_system: ActorSystem)
+  extends TestKit(_system)
+    with Matchers
+    with FlatSpecLike
+    with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("AkkaQuickstartSpec"))
+
+  override def afterAll: Unit = {
+    shutdown(system)
+  }
+
+  "A Device actor" should "reply with empty reading if no temperature is known" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
+
+    deviceActor.tell(Device.ReadTemperature(requestId = 42), probe.ref)
+    val response = probe.expectMsgType[Device.RespondTemperature]
+    response.requestId should ===(42)
+    response.value should ===(None)
+  }
+}
