4a8e971e3f7c1e69550de346ee1db6a553d6a66c
herb.nb-herb <herbert.kreutzer@gmail.com>
2018-01-29 15:05:52 +0100

    testkit update
      adhere scala 2.12.4 depcrecations

--- src/test/scala/at/co/sdt/herb/actors/doc/akka/io/actors/FSM/FSMDocSpec.scala
+++ src/test/scala/at/co/sdt/herb/actors/doc/akka/io/actors/FSM/FSMDocSpec.scala
@@ -145,7 +145,7 @@ class FSMDocSpec extends TestKit(ActorSystem("FSMDocSpec")) with ImplicitSender
     "not batch if uninitialized" in {
       val buncher = system.actorOf(Props[Buncher])
       buncher ! Queue(42)
-      expectNoMsg(1 second)
+      expectNoMessage(1 second)
     }
   }
 }
